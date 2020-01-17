cbf353f7f044d0e4e47fe41d816f51a2198365dd
seosd97 <seosd97@gmail.com>
2020-01-17 20:04:01 +0900

    기본 db스키마 정의

---
+++ MovieConnect/db/migrate/20200115235258_create_posts.rb
@@ -0,0 +1,15 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.references :user_id, foreign_key: true
+      t.string :movie_id
+      t.string :movie_name
+      t.string :movie_thumb
+      t.string :content
+      t.boolean :is_spoiler, default: false
+      t.integer :like_count
+      
+      t.timestamps
+    end
+  end
+end
