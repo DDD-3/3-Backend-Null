1a7225e929352c6e2e41c8c60f9619806695ea50
seosd97 <seosd97@gmail.com>
2020-01-23 21:15:22 +0900

    영화 id로 검색 기능 추가

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,15 @@ class MoviesController < ApplicationController
     require 'rest-client'
 
     def show
+        # NOTE : kmdb의 pk는 movieId + movieSeq으로 이루어짐
+        #        따라서 id를 전달받는 방식은 pk키인 docId로 전달받고, 파싱해서 movieId와 movieSeq로 검색하도록 함
+        #        movieSeq로만 검색하면 중복되는 값이 있기 때문에 위와 같이 검색함
+
+        movie_pk = params[:id]
         response = RestClient.get ENV["MOVIE_ENDPOINT"], params: {
             collection: "kmdb_new",
-            movieSeq: params[:id],
+            movieId: movie_pk[0],
+            movieSeq: movie_pk[1..],
             # 일반적인 제목 외에 더 정확한 정보를 위해서 키워드, 감독(박스오피스 등을 검색 후에 정보가 있는 경우), 제작/개봉일(박스오피스 등을 검색 후에 정보가 있는 경우)과 같은 정보를 추가해줘야 함
             ServiceKey: ENV["MOVIE_API_KEY"]
         }
