956dead3f36c69a4b3821d2586a7ca8d2f4e02a9
seosd97 <seosd97@gmail.com>
2020-01-23 19:00:49 +0900

    api키 숨김처리 및 gitignore파일 추가

--- MovieConnect/app/controllers/movies_controller.rb
+++ MovieConnect/app/controllers/movies_controller.rb
@@ -6,28 +6,17 @@ class MoviesController < ApplicationController
     end
 
     def search
-        response = RestClient.get "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json.jsp", params: {
+        response = RestClient.get ENV["MOVIE_ENDPOINT"], params: {
             collection: "kmdb_new",
             listCount: 1,
             title: params[:title],
             # 일반적인 제목 외에 더 정확한 정보를 위해서 키워드, 감독(박스오피스 등을 검색 후에 정보가 있는 경우), 제작/개봉일(박스오피스 등을 검색 후에 정보가 있는 경우)
-            ServiceKey: ""
+            ServiceKey: ENV["MOVIE_API_KEY"]
         }
 
         render json: response
     end
 
     def boxoffice
-    end
-
-    private
-    # 추후 보이지 않게 수정이 필요함
-    # 커밋할 때 지우고 커밋 필요
-    def API_KEY
-        return ""
-    end
-
-    def MOVIE_ENDPOINT
-        return "http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json.jsp"
     end
 end
--- MovieConnect/db/schema.rb
+++ MovieConnect/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_21_140105) do
+ActiveRecord::Schema.define(version: 2020_01_23_091045) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2020_01_21_140105) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "movie_like_count", default: 0
   end
 
   add_foreign_key "comments", "posts"
