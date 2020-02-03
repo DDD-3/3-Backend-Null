50eca556455ac53f5691cf661e410fc6cddfcb9b
seosd97 <seosd97@gmail.com>
2020-02-03 19:29:00 +0900

    api-only 서버 설정

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,9 @@ Bundler.require(*Rails.groups)
 
 module MovieConnect
   class Application < Rails::Application
+    # use api-only version
+    config.api_only = true;
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
@@ -15,5 +18,9 @@ module MovieConnect
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # additional middle ware
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
   end
 end
